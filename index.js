f0904107175d9bfa39a584fd8ac0623d024ee69b
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-08-06 19:24:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ console.log(movies)
 document.getElementById("Terminator 2").addEventListener("click", film)
 function film() {
   
-  
+document.getElementById("title").innerHTML = movies["Terminator 2"]["title"] 
+
+console.log(movies)  
   
 }
