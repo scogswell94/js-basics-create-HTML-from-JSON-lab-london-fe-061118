80b888b1ab1192becc3e501696cd482a9c4de34a
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-08-06 17:53:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentloaded", (e) => {
   console.log("LOADED")
-  // invoking a function here will make sure it happens on page load
-  // your code here
   document.getElementById("title").innerHTML=movies
   console.log(movies)
 })
